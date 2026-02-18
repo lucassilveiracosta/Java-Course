5a5359e31bb9da0675a4eccc2a44407ba5ed0ed6
lucassilveiracosta <lscosta2007@gmail.com>
2026-02-18 15:55:26 -0300

    End of the first exercise page

---
+++ basic/output-inputs/exercises/ex5.java
@@ -0,0 +1,41 @@
+package exercises;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex5 {
+    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        int cdg1, qntd1, cdg2, qntd2;
+        double preco1, preco2;
+
+        System.out.printf("Digite o código do produto 1\n> ");
+        cdg1 = sc.nextInt();
+        System.out.printf("Digite o quantidade do produto 1\n> ");
+        qntd1 = sc.nextInt();
+        System.out.printf("Digite o preço do produto 1\n> ");
+        preco1 = sc.nextDouble();
+
+        
+        
+        System.out.printf("\n\nDigite o código do produto 2\n> ");
+        cdg2 = sc.nextInt();
+        System.out.printf("Digite o quantidade do produto 2\n> ");
+        qntd2 = sc.nextInt();
+        System.out.printf("Digite o preço do produto 2\n> ");
+        preco2 = sc.nextDouble();
+
+
+        double pr_final1 = qntd1 * preco1;
+        double pr_final2 = qntd2 * preco2;
+        
+        System.out.printf("[%d] - R$ %.2f\n", cdg1, pr_final1);
+        System.out.printf("[%d] - R$ %.2f\n", cdg2, pr_final2);
+        System.out.printf("Total a pagar: R$ %.2f", pr_final1 + pr_final2);
+        sc.close();
+    }
+}
---
+++ basic/output-inputs/exercises/ex6.java
@@ -0,0 +1,35 @@
+package exercises;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex6 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        double A, B, C;
+
+        System.out.printf("type 3 float numbers on the arrow\n> ");
+        A = sc.nextDouble();
+        System.out.printf("\n> ");
+        B = sc.nextDouble();
+        System.out.printf("\n> ");
+        C = sc.nextDouble();
+
+        double areaTriangle = (A * C) / 2;
+        double areaCircle = 3.14159 * Math.pow(C, 2);
+        double areaTrapeze = ((A + B) * C) / 2;
+        double areaSquare = Math.pow(B, 2);
+        double areaRectangle = A * B;
+
+        System.out.printf("TRIANGLE: %.3f\n", areaTriangle);
+        System.out.printf("CIRCLE: %.3f\n", areaCircle);
+        System.out.printf("TRAPEZE: %.3f\n", areaTrapeze);
+        System.out.printf("SQUARE: %.3f\n", areaSquare);
+        System.out.printf("REACTANGLE: %.3f", areaRectangle);
+
+        sc.close();
+    }
+}
