4a26b99883b75349e1fa923f64797025a38267a5
lucassilveiracosta <lscosta2007@gmail.com>
2026-02-19 15:02:02 -0300

    End of the ex3

--- basic/loops/exercises/ex3.java
+++ basic/loops/exercises/ex3.java
@@ -7,10 +7,39 @@ public class ex3 {
         Scanner sc = new Scanner(System.in);
 
         int opcao = 1;
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
 
-        System.out.printf("Digite na seta o numero do combustivel utilizado pelo cliente\n> ");
+        System.out.printf("Digite na seta o numero do combustivel utilizado pelo cliente\n");
         while(opcao != 4){
+            System.out.printf("> ");
             opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    alcool++;
+                break;
+                case 2:
+                    gasolina++;
+                break;
+                case 3: 
+                    diesel++;
+                break;
+                case 4: 
+                    System.out.println("FIM");
+                break;
+            
+                default:
+                    System.out.println("Esse valor não existe!");    
+                break;
+            }
         }
+
+        System.out.printf("\nÁlcool: %d", alcool);
+        System.out.printf("\nGasolina: %d", gasolina);
+        System.out.printf("\nDiesel: %d", diesel);
+
+        sc.close();
     }
 }
