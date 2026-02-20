c3380bb08f7de4849b77d06a0418a8c1d4b2e6ad
lucassilveiracosta <lscosta2007@gmail.com>
2026-02-19 23:47:03 -0300

    Changes

--- basic/loops/exercises2/ex2.java
+++ basic/loops/exercises2/ex2.java
@@ -1,7 +1,33 @@
 package basic.loops.exercises2;
 
+import java.util.Scanner;
+
 public class ex2 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Quantos números inteiros você vai digitar?\n> ");
+        int qntd = sc.nextInt();
+
+        int num;
+        int in = 0;
+        int out = 0;
+        System.out.printf("Digite números\n");
+        for(int i = 0; i < qntd; i++){
+            System.out.print("> ");
+            num = sc.nextInt();
+
+            if(num >= 10 && num <= 20){
+                in++;
+            }
+            else{
+                out++;
+            }
+        }
+
+        System.out.printf("IN: %d\n", in);
+        System.out.printf("OUT: %d", out);
+
+        sc.close();
     }
 }
---
+++ basic/loops/exercises2/ex3.java
@@ -0,0 +1,7 @@
+package basic.loops.exercises2;
+
+public class ex3 {
+    public static void main(String[] args) {
+        
+    }
+}
