3158a39a67adce270d27cc9d145637d4ef1d5dbe
lucassilveiracosta <lscosta2007@gmail.com>
2026-03-12 23:39:30 -0300

    Salvando aqui, pois internet falhou, poucas mudanças

--- POO/Lista/ListaStudy.java
+++ POO/Lista/ListaStudy.java
@@ -1,11 +1,26 @@
 package POO.Lista;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListaStudy {
     public static void main(String[] args) {
         
-        List<Integer> list;
+        List<String> list; // Ainda temos que instaciar!!
         //     |-> Não pode ser tipo primitivo(int, double, char)
+
+        list = new ArrayList<>(); // Essa implemetação é otmizada, pega caracteristicas do vetor e da lista, daí ArrayList
+
+        list.add("Lucas");
+        list.add("Kimmy I");
+        list.add("Kimmy II");
+        list.add("Kimmy III");
+        list.add("Kimmy IV");
+
+        for(String obj: list) {
+            System.out.println(obj);
+        }
+
+
     }
 }
