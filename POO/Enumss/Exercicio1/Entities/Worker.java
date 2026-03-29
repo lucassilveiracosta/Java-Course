69ebc62bb11cc1e42234fc7537979c7974e78e34
lucassilveiracosta <lscosta2007@gmail.com>
2026-03-29 02:13:11 -0300

    Calendar

--- POO/Enumss/Exercicio1/Entities/Worker.java
+++ POO/Enumss/Exercicio1/Entities/Worker.java
@@ -2,16 +2,25 @@ package POO.Enumss.Exercicio1.Entities;
 
 import POO.Enumss.Exercicio1.Entities.Enums.WorkerLevel;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class Worker {
 
     private String name;
     private WorkerLevel workerLevel;
     private Double baseSalary;
 
-    public Worker(String name, WorkerLevel workerLevel, Double baseSalary) {
+    private String department;
+    private List<HourContract> contract = new ArrayList<>();
+
+    public Worker(String name, WorkerLevel workerLevel, Double baseSalary, String department) {
         this.name = name;
         this.workerLevel = workerLevel;
         this.baseSalary = baseSalary;
+        this.department = department;
     }
 
     public String getName() {
@@ -36,5 +45,31 @@ public class Worker {
 
     public void setBaseSalary(Double baseSalary) {
         this.baseSalary = baseSalary;
+    }
+
+    public void addContract(HourContract contract) {
+        this.contract.add(contract);
+    }
+
+    public void removeContract(HourContract contract) {
+        this.contract.remove(contract);
+    }
+
+    public Double income(Integer year, Integer month) {
+        double sum = baseSalary;
+        Calendar calendar = Calendar.getInstance();
+
+        for( HourContract c: contract ) {
+
+            calendar.setTime(c.getDate());
+            int c_year = calendar.get(Calendar.YEAR);
+            int c_month = calendar.get(Calendar.MONTH); // manipulaçao calendar, revisar a aula
+
+            if(c_year == year && c_month == month) {
+                sum += c.totalValue();
+            }
+        }
+
+        return sum;
     }
 }
