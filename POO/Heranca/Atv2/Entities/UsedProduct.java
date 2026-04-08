4d34f26583c5d62edf964f476c5682d106f3668e
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-08 20:34:27 -0300

    feat: implementa produto usado

--- POO/Heranca/Atv2/Entities/UsedProduct.java
+++ POO/Heranca/Atv2/Entities/UsedProduct.java
@@ -1,4 +1,29 @@
 package POO.Heranca.Atv2.Entities;
 
-public class UsedProduct {
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class UsedProduct extends Product{
+
+    private LocalDate manufactureDate;
+
+    public UsedProduct(String name, Double price, LocalDate manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    public LocalDate getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(LocalDate manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    @Override
+    public String priceTag() {
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return this.getName() + " (used) $ " + this.getPrice() + " (Manufacture date: " + this.getManufactureDate().format(fmt) + ")\n";
+    }
 }
