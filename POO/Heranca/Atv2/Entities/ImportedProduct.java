560f8dc3a01e0e7466cba710ca5e57d988e25c47
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-08 20:33:52 -0300

    feat: implementa produto importado

--- POO/Heranca/Atv2/Entities/ImportedProduct.java
+++ POO/Heranca/Atv2/Entities/ImportedProduct.java
@@ -1,4 +1,28 @@
 package POO.Heranca.Atv2.Entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product{
+
+    private Double customsFee;
+
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    public Double totalPrice() {
+        return this.getPrice() + this.getCustomsFee();
+    }
+
+    @Override
+    public String priceTag() {
+        return this.getName() + " $ " + this.getPrice() + " (Customs fee: $ " + this.getCustomsFee() + ")\n";
+    }
 }
