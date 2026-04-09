d2a7a31f098724f7d4578d96ddbb8684a5318190
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-08 21:16:59 -0300

    feat: implementa circle

---
+++ POO/Heranca/Abstracts/Entities/Circle.java
@@ -0,0 +1,28 @@
+package POO.Heranca.Abstracts.Entities;
+
+import POO.Heranca.Abstracts.Enum.Color;
+
+public class Circle extends Shape{
+
+    protected Double radius;
+
+    private final Double pi = 3.14;
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public Double area() {
+        return pi * Math.pow(this.radius, 2);
+    }
+}
