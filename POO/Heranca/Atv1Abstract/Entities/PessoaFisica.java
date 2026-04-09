a4d5aa9ecf106fc308b71255549f316e3fcbd023
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-08 22:22:33 -0300

    feat: implementa pessoa fisica

--- POO/Heranca/Atv1Abstract/Entities/PessoaFisica.java
+++ POO/Heranca/Atv1Abstract/Entities/PessoaFisica.java
@@ -1,4 +1,37 @@
 package POO.Heranca.Atv1Abstract.Entities;
 
-public class PessoaFisica {
+public class PessoaFisica extends Pessoa {
+    private Double gastosComSaude;
+
+    public PessoaFisica(String nome, Double rendaAnual, Double gastosComSaude) {
+        super(nome, rendaAnual);
+        this.gastosComSaude = gastosComSaude;
+    }
+
+    public Double getGastosComSaude() {
+        return gastosComSaude;
+    }
+
+    public void setGastosComSaude(Double gastosComSaude) {
+        this.gastosComSaude = gastosComSaude;
+    }
+
+    @Override
+    public Double imposto() {
+        if(gastosComSaude > 0) {
+            if (rendaAnual < 20000.00) {
+                return (rendaAnual * 0.15) - (gastosComSaude * 0.5);
+            } else {
+                return (rendaAnual * 0.25) - (gastosComSaude * 0.5);
+            }
+        }
+        else {
+            if (rendaAnual < 20000.00) {
+                return (rendaAnual * 0.15);
+            } else {
+                return (rendaAnual * 0.25);
+            }
+        }
+    }
+
 }
