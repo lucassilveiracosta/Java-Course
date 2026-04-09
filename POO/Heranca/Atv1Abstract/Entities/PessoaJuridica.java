a5205c7ad42286463b6cf11b9c98b23c486711d4
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-08 22:22:14 -0300

    feat: implementa pessoa juridica

--- POO/Heranca/Atv1Abstract/Entities/PessoaJuridica.java
+++ POO/Heranca/Atv1Abstract/Entities/PessoaJuridica.java
@@ -1,4 +1,29 @@
 package POO.Heranca.Atv1Abstract.Entities;
 
-public class PessoaJuridica {
+public class PessoaJuridica extends Pessoa{
+
+    private Integer numDeFuncionarios;
+
+    public PessoaJuridica(String nome, Double rendaAnual, Integer numDeFuncionarios) {
+        super(nome, rendaAnual);
+        this.numDeFuncionarios = numDeFuncionarios;
+    }
+
+    public Integer getNumDeFuncionarios() {
+        return numDeFuncionarios;
+    }
+
+    public void setNumDeFuncionarios(Integer numDeFuncionarios) {
+        this.numDeFuncionarios = numDeFuncionarios;
+    }
+
+    @Override
+    public Double imposto() {
+        if(numDeFuncionarios > 10) {
+            return rendaAnual * 0.14;
+        }
+        else {
+            return rendaAnual * 0.16;
+        }
+    }
 }
