11477e502e850657164b413ec8d2eebe9323a07f
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-12 11:04:59 -0300

    feat: studying try-catch

---
+++ POO/Exception/Test.java
@@ -0,0 +1,28 @@
+package POO.Exception;
+
+import javax.xml.transform.Source;
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Test {
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int pos = sc.nextInt();
+            System.out.println(vect[pos]); // possiveis erros (input mismatch, indexoutodbound)
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Posição invalida");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("A posição deve ser inteira");
+        }
+
+        System.out.println("Fim do programa");
+        sc.close();
+    }
+}
