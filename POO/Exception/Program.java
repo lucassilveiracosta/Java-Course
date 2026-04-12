a26356a2f5c5cc319afc2756b0d8dda421ce3918
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-12 11:27:37 -0300

    feat: leitor de arquivo

--- POO/Exception/Program.java
+++ POO/Exception/Program.java
@@ -1,4 +1,27 @@
 package POO.Exception;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Program {
+    static void main(String[] args) {
+
+        File file = new File("C:\\Users\\lscos\\OneDrive\\Documentos\\Projetos Internos\\Hands-on-Java\\POO\\Exception\\file.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error opening file " + e.getMessage());
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
 }
