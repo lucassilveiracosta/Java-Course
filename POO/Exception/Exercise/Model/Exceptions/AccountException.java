c04090273c2864cb2a72bb046fef13cb7e28b8ba
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-12 16:26:09 -0300

    feat: implementa classe de excessões para a conta

--- POO/Exception/Exercise/Model/Exceptions/AccountException.java
+++ POO/Exception/Exercise/Model/Exceptions/AccountException.java
@@ -1,7 +1,7 @@
 package POO.Exception.Exercise.Model.Exceptions;
 
 public class AccountException extends RuntimeException {
-  public AccountException(String message) {
-    super(message);
-  }
+    public AccountException(String message) {
+        super(message);
+    }
 }
