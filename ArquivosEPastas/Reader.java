46ca7e37183088d208688853f1fba976c958b937
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-12 22:29:27 -0300

    feat: abrindo arquivos e fechando com fileReader e bufferedReader

---
+++ ArquivosEPastas/Reader.java
@@ -0,0 +1,44 @@
+package ArquivosEPastas;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader {
+
+    static void main(String[] args) {
+
+        String path = "C:\\Users\\lscos\\OneDrive\\Documentos\\Projetos Internos\\Hands-on-Java\\ArquivosEPastas\\in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
