6baa12941ddf6484a58f6a56812761cd4f45a0c5
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-12 22:42:56 -0300

    feat: criando arquivo e escrevendo

---
+++ ArquivosEPastas/Writeee.java
@@ -0,0 +1,23 @@
+package ArquivosEPastas;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Writeee {
+    static void main(String[] args) {
+
+        String[] lines = new String[] { "Good Morning", "Good Afternoon", "Good Night"};
+        String path = "C:\\Users\\lscos\\OneDrive\\Documentos\\Projetos Internos\\Hands-on-Java\\ArquivosEPastas\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter( new FileWriter(path))){
+            for (String line: lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
