f048b711f871c9210b301ea2a65069b11d23aaac
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-12 22:19:21 -0300

    feat: file reader, learning whats happening in the code

--- ArquivosEPastas/LeitorDeArquivo.java
+++ ArquivosEPastas/LeitorDeArquivo.java
@@ -1,4 +1,28 @@
 package ArquivosEPastas;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class LeitorDeArquivo {
+
+    static void main(String[] args) {
+
+        File file = new File("C:\\Users\\lscos\\OneDrive\\Documentos\\Projetos Internos\\Hands-on-Java\\ArquivosEPastas\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            if(sc != null) {
+                sc.close();
+            }
+        }
+    }
 }
