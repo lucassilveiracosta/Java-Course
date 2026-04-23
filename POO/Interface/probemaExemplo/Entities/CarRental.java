fbf9e56db6529046ad75d1300478fafe51fa8cfa
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-22 22:02:03 -0300

    feat: get and set CarRental

--- POO/Interface/probemaExemplo/Entities/CarRental.java
+++ POO/Interface/probemaExemplo/Entities/CarRental.java
@@ -9,4 +9,42 @@ public class CarRental {
     private Vehicle vehicle;
     private CarRental carRental;
 
+    public CarRental(LocalDateTime start, LocalDateTime finish, Vehicle vehicle, CarRental carRental) {
+        this.start = start;
+        this.finish = finish;
+        this.vehicle = vehicle;
+        this.carRental = carRental;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getFinish() {
+        return finish;
+    }
+
+    public void setFinish(LocalDateTime finish) {
+        this.finish = finish;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public CarRental getCarRental() {
+        return carRental;
+    }
+
+    public void setCarRental(CarRental carRental) {
+        this.carRental = carRental;
+    }
 }
