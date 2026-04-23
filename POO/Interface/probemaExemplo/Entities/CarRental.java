2d881ecfb559b51dbe0ac4e482d4d1daa4f8988f
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-22 22:01:29 -0300

    feat: class invoice

---
+++ POO/Interface/probemaExemplo/Entities/CarRental.java
@@ -0,0 +1,12 @@
+package POO.Interface.probemaExemplo.Entities;
+
+import java.time.LocalDateTime;
+
+public class CarRental {
+
+    private LocalDateTime start;
+    private LocalDateTime finish;
+    private Vehicle vehicle;
+    private CarRental carRental;
+
+}
