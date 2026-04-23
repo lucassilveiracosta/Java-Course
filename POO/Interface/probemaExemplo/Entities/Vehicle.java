a350bcf5ac8c9dfd0160abfdd29cbd97b1efc4d1
lucassilveiracosta <lscosta2007@gmail.com>
2026-04-22 21:57:03 -0300

    feat: class Vehicle

---
+++ POO/Interface/probemaExemplo/Entities/Vehicle.java
@@ -0,0 +1,18 @@
+package POO.Interface.probemaExemplo.Entities;
+
+public class Vehicle {
+
+    private String model;
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
